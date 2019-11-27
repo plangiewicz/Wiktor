58b8d5a0ccff73449f02679e83a2b66df2d8cf93
plangiewicz <plangiewicz@hotmail.com>
2019-11-27 22:43:53 +0100

    Start commit

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public GameObject[] cloths;
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void SetCloth(int c)
+    {
+        foreach (var cloth in cloths)
+        {
+            cloth.SetActive(false);
+        }
+        cloths[c].SetActive(true);
+    }
+
+    public void ChangeAnimation()
+    {
+        _animator.SetTrigger("change");
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] characters;
+
+    public void SetCharacter(int c)
+    {
+        foreach (var character in characters)
+        {
+            character.SetActive(false);
+        }
+        characters[c].SetActive(true);
+    }
+
+    public void SetCloth(int c)
+    {
+        foreach (var character in characters)
+        {
+            character.GetComponent<CharacterController>().SetCloth(c);
+        }
+    }
+
+    public void ChangeAnimation()
+    {
+        foreach (var character in characters)
+        {
+            character.GetComponent<CharacterController>().ChangeAnimation();
+        }
+    }
+}
